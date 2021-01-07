819d4561a2bb081db78a030bc8e40f15fe13b132
quangmindx <quangtmps12135@fpt.edu.vn>
2021-01-07 15:47:43 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Trần Minh Quang on 07/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
